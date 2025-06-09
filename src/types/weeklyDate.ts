025694f1a47fb08ef015317e2990914d41d0371d
Zahra-Shadman <zahra.shadmani.work@gmail.com>
2025-06-10 00:45:07 +0330

    define types

---
+++ src/types/weeklyDate.ts
@@ -0,0 +1,28 @@
+
+export interface IWeatherItem {
+  dt: number;
+  main: {
+    temp: number;
+    temp_max: number;
+    temp_min: number;
+  };
+  weather: {
+    description: string;
+    icon: string;
+  }[];
+  dt_txt: string;
+}
+
+export interface IWeeklyWeatherData {
+  list: IWeatherItem[];
+  city: {
+    name: string;
+  };
+}
+
+export interface IWeeklyDataFetcherProps {
+  city: string;
+  onDataFetched: (data: IWeeklyWeatherData | null) => void;
+  onError: (error: string | null) => void;
+  onLoading: (loading: boolean) => void;
+}
